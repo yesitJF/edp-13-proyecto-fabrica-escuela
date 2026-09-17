9b787acdd154db6b79b8a127d7e7f66cfbb664ad
Juan Ramirez <juanesrmz98@gmail.com>
2026-09-16 20:39:21 -0500

    Initial commit: Configuración inicial de Spring Boot con PostgreSQL

---
+++ src/main/java/ebp13/fabrica/escuela/ebp13_fabrica_escuela/Ebp13FabricaEscuelaApplication.java
@@ -0,0 +1,13 @@
+package ebp13.fabrica.escuela.ebp13_fabrica_escuela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ebp13FabricaEscuelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ebp13FabricaEscuelaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ebp13/fabrica/escuela/ebp13_fabrica_escuela/Ebp13FabricaEscuelaApplicationTests.java
@@ -0,0 +1,13 @@
+package ebp13.fabrica.escuela.ebp13_fabrica_escuela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ebp13FabricaEscuelaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
